74ea0cbd28d4b21aef7ce932a902dfad6e8b674a
Juan14-04 <juandallow1@gmail.com>
2025-10-15 13:59:44 -0500

    se agrego funcionalidad

--- src/main/java/com/example/GestiondeTareasAcademicas/entities/Estudiante.java
+++ src/main/java/com/example/GestiondeTareasAcademicas/entities/Estudiante.java
@@ -1,4 +1,25 @@
 package com.example.GestiondeTareasAcademicas.entities;
 
 public class Estudiante {
+    private String name;
+    private String id;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+
+
+
 }
+
--- src/main/java/com/example/GestiondeTareasAcademicas/entities/Tarea.java
+++ src/main/java/com/example/GestiondeTareasAcademicas/entities/Tarea.java
@@ -1,4 +1,50 @@
 package com.example.GestiondeTareasAcademicas.entities;
 
 public class Tarea {
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String completada;
+
+    public Tarea() {
+    }
+
+    public Tarea(Long id, String titulo, String descripcion, String completada) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.completada = completada;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(String completada) {
+        this.completada = completada;
+    }
 }
