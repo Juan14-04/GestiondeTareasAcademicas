2862fef94ebdad2a37618a54de868256bed09790
Juan14-04 <juandallow1@gmail.com>
2025-10-15 15:53:49 -0500

    Change API endpoint from '/api' to '/api2'

--- src/main/java/com/example/GestiondeTareasAcademicas/controller/TareaController.java
+++ src/main/java/com/example/GestiondeTareasAcademicas/controller/TareaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping("/api")
+@RequestMapping("/api2")
 @RestController
 public class TareaController {    private ImTareaService imTareaService;
     @PostMapping("/crear")
